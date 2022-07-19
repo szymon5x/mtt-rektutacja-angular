9a62cb0bf31dff2f35a4a4d5fb8334fcd0b127ed
szymon5x <szymon5x@wp.pl>
2022-07-19 13:29:39 +0200

    english version prep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import * as components from './components';
 
 @NgModule({
   declarations: [AppComponent, components.DashboardComponent, components.HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,24 +7,19 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit {
-  displayedInstructions: string[] = [];
+  displayedInstructions: string[] = [
+    'Create a "todo-list" component',
+    'Register the todo-list component in module under `todo-list` route',
+    'Create "todo-list" service',
+    'Add a method to the service that retrieves data from endpoint https://gorest.co.in/public/v2/todos',
+    'Display the data downloaded by the website in the created component',
+    'Display date in "29-06-2022" format',
+    'In DashboardComponent, combine "one" and "two" arrays. Remove duplicate elements. Display result ',
+    'Count how many times each status downloaded by the website occurs. Display this data in todo-list.component.html'
+  ];
 
   ngOnInit(): void {
-    const instructions = [
-      'Stwórz komponent "todo-list"',
-      'W header.component do linku dodaj ścieżkę, pod którą zarejestruj komponent todo-list',
-      'Stwórz serwis "todo-list"',
-      'Dodaj w serwisie metodę pobierającą dane z endpointu https://gorest.co.in/public/v2/todos',
-      'W komponencie "Dashboard" do tablicy "displayedInstructions" dodaj instrukcje znajdujące się w tablicy "instructions" oraz "nextInstructions"'
-    ];
-    const nextInstructions = [
-      'Zrób serwis jako "provided in todo-list.component"',
-      'Wyświetl w stworzonym komponencie dane pobierane przez serwis',
-      'Dodaj funkcję trackBy',
-      'Sformatuj datę do postaci "29-06-2022"',
-      'Policz ile razy występuje każdy z pobieranych przez serwis statusów. Wyświetl te dane w todo-list.component.html'
-    ];
-
-    this.displayedInstructions = instructions;
+    const one = ['1', '2', '3', '4', '5'];
+    const two = ['6', '7', '2', '8', '5'];
   }
 }
